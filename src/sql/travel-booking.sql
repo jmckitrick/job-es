75181a0396adcd30cb90cee443b37e1c6171a5a6
Jonathon McKitrick <jonathon.mckitrick@tstllc.net>
2019-07-02 09:04:36 -0400

    Experiment with query string and highlight

--- src/sql/travel-booking.sql
+++ src/sql/travel-booking.sql
@@ -17,7 +17,7 @@ SELECT
 FROM TRAVEL_BOOKING tb
 INNER JOIN TRAVELLER t ON t.booking_id = tb.id
 WHERE t.is_primary = 1
-AND tb.create_date >= '2019-06-01 00:00:00'
+-- AND tb.create_date >= '2019-06-01 00:00:00'
 -- AND tb.create_date <  '2019-06-08 00:00:00'
 -- AND tb.create_date >= '2019-06-08 00:00:00'
 -- AND tb.create_date <  '2019-06-15 00:00:00'
@@ -28,4 +28,6 @@ AND tb.create_date >= '2019-06-01 00:00:00'
 -- AND tb.agent_name IS NOT NULL
 -- AND tb.subsite_agent_name IS NOT NULL
 -- AND (tb.agent_name IS NOT NULL OR tb.subsite_agent_name IS NOT NULL)
+AND tb.create_date >= '2019-05-01 00:00:00'
+-- AND tb.create_date <  '2019-05-01 00:00:00'
 -- LIMIT 1000
