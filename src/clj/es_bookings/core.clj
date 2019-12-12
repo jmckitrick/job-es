dcd1313c163f4137d766fb2181cef2199b9274bc
Jonathon McKitrick <jonathon.mckitrick@tstllc.net>
2019-12-12 07:37:52 -0500

    Snapshot before importing more data

--- src/clj/es_bookings/core.clj
+++ src/clj/es_bookings/core.clj
@@ -10,40 +10,52 @@
   "Get a database connection spec.
   Prefer env variables then fall back to defaults."
   []
-  (let [hostname (or (System/getenv "DB_HOST") "mysqlread.prod.infra.tstllc.net")
-        username (or (System/getenv "DB_USER") "support_ro")
-        password (or (System/getenv "DB_PASS") "1mdsupp0rtp3rs0n!")]
+  (let [;;hostname (or (System/getenv "DB_HOST") "mysqlread.prod.infra.tstllc.net")
+        ;;username (or (System/getenv "DB_USER") "support_ro")
+        ;;password (or (System/getenv "DB_PASS") "1mdsupp0rtp3rs0n!")
+        hostname (or (System/getenv "DB_HOST") "nodes.lower.kube.tstllc.net")
+        username (or (System/getenv "DB_USER") "root")
+        password (or (System/getenv "DB_PASS") "kube-aws")
+        port 32110]
     {:dbtype "mysql"
      :dbname "book"
      :user username
      :password password
      :host hostname
-     :ssl false
-     }))
+     :port 32110
+     :ssl false}))
 
 (defqueries "travel-booking.sql" {:connection (get-db-spec)})
 
-(def c (s/client {:hosts ["http://localhost:8000"]}))
+;;(def c (s/client {:hosts ["http://localhost:8000"]}))
+(def c (s/client {:hosts ["http://nodes.lower.kube.tstllc.net:32200"]}))
 
 (defn get-booking-type [row]
   (cond
-    (and (not-empty (:agent row))
-         (empty? (:subsite row))) "agent"
-    (and (not-empty (:subsite row))
-         (empty? (:agent row))) "personal subsite"
+    (and (empty? (:subsite row))
+         (not-empty (:agent row))) "agent"
+    (and (empty? (:agent row))
+         (not-empty (:subsite row))) "personal subsite"
     :else "web"))
 
 (defn add-to-index-bulk-async [rows]
   (let [{:keys [input-ch output-ch]} (s/bulk-chan c {:flush-threshold 100
                                                      :flush-interval 5000
                                                      :max-concurrent-requests 1000})]
-    (doseq [row rows]
-      (let [ready-row (assoc row :type (get-booking-type row))]
-        (async/>!! input-ch [{:index {:_index :booking :_type :_doc :_id (:id row)}} ready-row])))
+    (doseq [row-set (partition-all 1000 rows)]
+      ;;(println "Importing " (count row-set))
+      (doseq [row row-set]
+        (let [ready-row (assoc row :booking_type (get-booking-type row))]
+          (async/>!! input-ch [{:index  ;desired ES action
+                                {:_index :booking ;name of index
+                                 :_type :_doc ;add to documents on index
+                                 :_id (:id row)}}
+                               ready-row]))))
     (future (loop [] (async/<!! output-ch)))))
 
 (defn -main []
-  (let [bookings (get-travel-bookings)]
+  (let [bookings (get-travel-bookings {:start_date "2019-01-01 00:00:00"
+                                       :end_date "2019-02-01 00:00:00"})]
     (println "Bookings: " (count bookings))
     #_(println "Bookings: " bookings)
     (add-to-index-bulk-async bookings)))
--- src/sql/travel-booking.sql
+++ src/sql/travel-booking.sql
@@ -1,9 +1,10 @@
 -- name: get-travel-bookings
--- Get travel bookings from June 2019 onward.
+-- Get columns for elastic search from travel bookings.
 SELECT
  tb.id,
  tb.licensee_id licensee,
  tb.product_type product,
+ tb.product_sub_type product_sub_type,
  tb.adapter_name adapter,
  tb.agent_name agent,
  tb.subsite_agent_name subsite,
@@ -17,7 +18,9 @@ SELECT
 FROM TRAVEL_BOOKING tb
 INNER JOIN TRAVELLER t ON t.booking_id = tb.id
 WHERE t.is_primary = 1
--- AND tb.create_date >= '2019-06-01 00:00:00'
+AND tb.create_date >= :start_date
+AND tb.create_date <  :end_date
+-- AND tb.create_date >= '2019-01-01 00:00:00'
 -- AND tb.create_date <  '2019-06-08 00:00:00'
 -- AND tb.create_date >= '2019-06-08 00:00:00'
 -- AND tb.create_date <  '2019-06-15 00:00:00'
@@ -28,6 +31,6 @@ WHERE t.is_primary = 1
 -- AND tb.agent_name IS NOT NULL
 -- AND tb.subsite_agent_name IS NOT NULL
 -- AND (tb.agent_name IS NOT NULL OR tb.subsite_agent_name IS NOT NULL)
-AND tb.create_date >= '2019-05-01 00:00:00'
--- AND tb.create_date <  '2019-05-01 00:00:00'
+-- AND tb.create_date >= '2019-07-01 00:00:00'
+-- AND tb.create_date <  '2019-08-01 00:00:00'
 -- LIMIT 1000
